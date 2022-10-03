60e817c0271d5d76df18432917dcbf93e7f8166f
Vaibhav <Vaibhav@example.com>
2022-10-03 12:59:39 +0530

    adding mode json file for the server

---
+++ db.js
@@ -0,0 +1,9 @@
+const items = require("./data/items.json");
+const account = require("./data/Account.json");
+const buyer = require("./data/db.json");
+
+module.exports = {
+  items,
+  account,
+  buyer,
+};
